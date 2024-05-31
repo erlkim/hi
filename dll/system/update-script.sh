255b03d668841e25c18d676d786209b42e0ca691
Erlkim <99765959+erlkim@users.noreply.github.com>
2024-06-01 01:04:29 +0700

    Update update-script.sh

--- dll/system/update-script.sh
+++ dll/system/update-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -q -O /usr/bin/system-menu "https://raw.githubusercontent.com/erlkim/hi/main/serv-updater.sh" && chmod +x /usr/bin/yow
+wget -q -O /usr/bin/yow "https://raw.githubusercontent.com/erlkim/hi/main/serv-updater.sh" && chmod +x /usr/bin/yow
 screen -S updss yow
